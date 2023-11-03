b8d6dc49ca1d9d9d58a2c24f067b030e7aa87038
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-11-03 21:43:38 +0500

    added the fetch functionality from the button

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData } from "./DataFetching/FetchSlice";
 
 function Input() {
+  const data = useSelector((state) => state.fetchData.data);
+
+  console.log(data);
+
+  const dispatch = useDispatch();
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    dispatch(fetchData());
+  };
   return (
     <div className="w-full h-16  rounded-xl flex flex-row hover:border-4 transition-all ">
       <input
@@ -8,7 +20,7 @@ function Input() {
         className="w-full  flex justify-center align-center bg-transparent pl-2 text-[#cccccc] outline-0  w-full h-full rounded-xl"
         placeholder="Search a word"
       />
-      <button className="w-12 h-full  rounded-xl">
+      <button className="w-12 h-full  rounded-xl " onClick={handleClick}>
         <img
           src="../../public/assets/icon-search.svg"
           alt="search"
