3ef8edf3661d84f0a6a1e412fc92636d64f6fae3
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-11-03 21:39:43 +0500

    completed the input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 function Input() {
   return (
-    <div className="w-full h-12 bg-red-900 rounded-xl">
+    <div className="w-full h-16  rounded-xl flex flex-row hover:border-4 transition-all ">
       <input
         type="text"
-        className="w-full bg-[#0d1829] flex bg-transparent pl-2 text-[#cccccc] outline-0"
+        className="w-full  flex justify-center align-center bg-transparent pl-2 text-[#cccccc] outline-0  w-full h-full rounded-xl"
         placeholder="Search a word"
       />
+      <button className="w-12 h-full  rounded-xl">
+        <img
+          src="../../public/assets/icon-search.svg"
+          alt="search"
+          className="h-3/5 w-full"
+        />
+      </button>
     </div>
   );
 }
