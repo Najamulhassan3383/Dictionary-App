652f46030fb09fc30128bee74a6b9b83730c8aea
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-11-04 21:58:02 +0500

    added the audio play button and functionality

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -3,31 +3,46 @@ import { useSelector } from "react-redux";
 
 function Sound() {
   const data = useSelector((state) => state.fetchData.data);
-  let word;
-  if (!data) {
-    word = "hello";
-  } else {
-    word = data[0].word;
+  let state = {
+    word: "hello",
+    url: "",
+  };
+
+  if (data) {
+    state = {
+      word: data.word,
+      url: data[0].phonetics[0].audio,
+    };
   }
+
+  const audio = new Audio(state.url);
+  audio.loop = false;
 
   const [playing, setPlaying] = React.useState(false);
   const audioRef = React.useRef(null);
   const toggle = () => setPlaying(!playing);
+  console.log(state.url);
   React.useEffect(() => {
+    console.log(audioRef.current);
     playing ? audioRef.current.play() : audioRef.current.pause();
   }, [playing]);
   return (
     <div className="w-full my-6 flex flex-row justify-between">
       <div>
-        <h2 className="text-2xl font-bold">{word}</h2>
+        <h2 className="text-2xl font-bold">{state.word}</h2>
         <p className="text-xl">Pronounce</p>
       </div>
       <div>
-        <audio className="w-full" ref={audioRef}>
-          <source src="horse.ogg" type="audio/ogg" />
-          <source src="horse.mp3" type="audio/mpeg" />
+        <audio className="w-full" ref={audioRef} muted>
+          <source src={state.url} type="audio/ogg" />
         </audio>
-        <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+        <button
+          onClick={() => {
+            audio.play();
+          }}
+        >
+          {playing ? "Pause" : "Play"}
+        </button>
       </div>
     </div>
   );
