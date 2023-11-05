1fd24a69607720cb59c5bf12425a30d1944cf312
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-11-05 12:05:15 +0500

    fixed the sound errors

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -3,49 +3,41 @@ import { useSelector } from "react-redux";
 
 function Sound() {
   const data = useSelector((state) => state.fetchData.data);
-  let state = {
-    word: "hello",
-    url: "",
-  };
-
+  let audio_src;
+  let word;
+  let text;
   if (data) {
-    let newurl = data[0].phonetics.filter((item) => {
-      return item.audio !== "";
-    });
-    let text = data[0].phonetics.filter((item) => {
-      console.log("from text", item.text);
-      if (item.text) {
-        return item;
+    for (let i = 0; i < data[0].phonetics.length; i++) {
+      if (data[0].phonetics[i].audio) {
+        audio_src = data[0].phonetics[i].audio;
       }
-    });
-    state = {
-      word: data[0].word,
-      url: newurl[0].audio,
-      text: text[0].text,
-    };
-    console.log(text[0].text);
+      if (data[0].phonetics[i].text) {
+        text = data[0].phonetics[i].text;
+      }
+      word = data[0].word;
+    }
   }
 
-  const audio = new Audio(state.url);
+  const audio = new Audio(audio_src);
   audio.loop = false;
 
   const [playing, setPlaying] = React.useState(false);
   const audioRef = React.useRef(null);
   const toggle = () => setPlaying(!playing);
-  console.log(state.url);
+  // console.log(state.url);
   React.useEffect(() => {
-    console.log(audioRef.current);
+    // console.log(audioRef.current);
     playing ? audioRef.current.play() : audioRef.current.pause();
   }, [playing]);
   return (
     <div className="w-full my-6 flex flex-row justify-between">
       <div>
-        <h2 className="text-6xl font-bold lowercases my-2">{state.word}</h2>
-        <p className="text-xl">{state.text}</p>
+        <h2 className="text-6xl font-bold lowercases my-2">{word}</h2>
+        <p className="text-xl">{text}</p>
       </div>
       <div>
         <audio className="w-full" ref={audioRef} muted>
-          <source src={state.url} type="audio/ogg" />
+          <source src={audio_src} type="audio/ogg" />
         </audio>
         <button
           onClick={() => {
