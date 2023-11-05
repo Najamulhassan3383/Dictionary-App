49c62bcf02fc7823f3e6b39beb03539dbd28ed1e
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-11-05 11:50:00 +0500

    complelted the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,22 +4,34 @@ import { useSelector } from "react-redux";
 import MeaningWrapper from "./MeaningWrapper";
 
 function Hero() {
-    const data = useSelector((state) => state.fetchData.data);
-    
+  const data = useSelector((state) => state.fetchData.data);
+  console.log(data);
 
   return (
-    
-    <div>
-      <Sound />
-      {meaning_data.map((item) => {
-        return (
-          <MeaningWrapper
-            key={item.partOfSpeech}
-            type={item.partOfSpeech}
-            data={item.definitions}
-          />
-        );
-      })}
+    <div className="w-full h-full">
+      {data ? (
+        <div>
+          <Sound />
+          {data[0].meanings.map((item, index) => (
+            <MeaningWrapper
+              key={index}
+              type={item.partOfSpeech}
+              data={item.definitions}
+            />
+          ))}
+          <div className="w-full flex flex-row justify-center items-center  my-8">
+            <p className="text-2xl mr-4  uppercase">Source</p>
+            <div className="w-full h-[1px] bg-red-900 "></div>
+          </div>
+          <p className="text-xl  italic mb-4">{data[0].sourceUrls}</p>
+        </div>
+      ) : (
+        <img
+          src="../../public/assets/Loading.svg"
+          alt="loading"
+          className="w-24 h-24 my-8 mx-auto"
+        />
+      )}
     </div>
   );
 }
--- src/components/MeaningWrapper.jsx
+++ src/components/MeaningWrapper.jsx
@@ -3,23 +3,25 @@ import PropTypes from "prop-types";
 
 function MeaningWrapper({ type, data }) {
   return (
-    <div className="my-4 w-full ">
+    <div className="my-6 w-full ">
       {/* for type */}
-      <div className="w-full flex flex-row ">
-        <p className="text-2xl font-bold">{type}</p>
-        <div className="w-full h-1 bg-[#cccccc]"></div>
+      <div className="w-full flex flex-row justify-center items-center  my-8">
+        <p className="text-2xl mr-4  uppercase">{type}</p>
+        <div className="w-full h-[1px] bg-red-900 "></div>
       </div>
 
       {/* for meaning? */}
       <div className="my-4 w-full">
-        <p className="text-xl">Meaning</p>
-        {data.map((item, index) => {
-          return (
-            <div key={index} className="my-2">
-              <p className="text-xl font-bold">{item.definition}</p>
-            </div>
-          );
-        })}
+        <p className="text-xl  italic mb-4">Meaning</p>
+        <ul className="list-disc">
+          {data.map((item, index) => {
+            return (
+              <li key={index} className="my-2">
+                <p className="text-lg ">{item.definition}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
